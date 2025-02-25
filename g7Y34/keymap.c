4aae7bad34b6efa206f2237d2259d889db45416a
Matt Britt <msbritt@gmail.com>
2025-02-24 22:11:42 -0500

    Fix incorrect paste

--- g7Y34/keymap.c
+++ g7Y34/keymap.c
@@ -84,9 +84,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (!process_achordion(keycode, record)) { return false; }
+	
   switch (keycode) {
-    if (!process_achordion(keycode, record)) { return false; }
-
     case RGB_SLD:
       if (record->event.pressed) {
         rgblight_mode(1);
